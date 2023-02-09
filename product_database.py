247ed52f76c00ca144458ae89e754779645f7ea3
Yoel Gebrehiwet <yoelg@uw.edu>
2023-02-09 13:31:18 -0800

    Setting up functions

--- product_database.py
+++ product_database.py
@@ -28,4 +28,35 @@ def get_products(category):
         dict_list.append(row_dict)
     return dict_list
 
+# Return all the category names
+def get_product_categories():
+    cat_list = []
+    stmt = "Select * from product_categories"
+    db_cursor.execute(stmt)
+    result_set = db_cursor.fetchall()
+    for row in result_set:
+        cat_list.append(row)
+    return cat_list
+
+def create_user():
+    pass
+
+def create_user_review():
+    pass
+
+def update_quantity():
+    pass
+
+def delete_user():
+    pass
+
+def get_product_by_price():
+    pass
+
+def get_review_by_pop():
+    pass
+
+def get_all_products():
+    pass
+
 # print(get_products("Fruits"))
