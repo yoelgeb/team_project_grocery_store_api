af66c3478f7a5871027fa6238a36b8c1f35f91e6
Yoel <73871471+yoelgeb@users.noreply.github.com>
2023-02-09 12:38:35 -0800

    fixing

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
+from product_database import get_products
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/", methods= ["GET", "POST"])
 def home():
-    return render_template("home.html", title = 'Home')
+    return render_template("home.html", title = 'Home', result=get_products("Fruits"))
 
 
 
